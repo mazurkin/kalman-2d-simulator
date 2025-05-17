4962eaf2abc569a0b9361887ca04be7f5f381987
Nick Mazurkin <mazurkin@gmail.com>
2025-05-17 10:22:02 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# Kalman filter in 2d simulation
+
+With some help of ChatGPT
+
+![Kalman 2D simulation](doc/simulator.png)
+
+## installation
+
+Requires [Conda](https://anaconda.org/anaconda/conda)
+
+```shell
+# create the new Conda environment
+make env-init
+
+# install the packages with Poetry
+make env-create
+```
+
+## run
+
+Use the keys:
+- the arrow keys to increase/decrease acceleration of the object
+- `r` to reset the object to the initial position
+- `s` to stop the object
+- `esc` to exit
+
+Indicators:
+- real position of the object: `blue`
+- noisy measurement: `red`
+- estimation from the Kalman filter: `green`
+- median and mean averaging: `gray`
+
+```shell
+# run the simulation
+make run
+```
---
+++ src/simulator.py
@@ -0,0 +1,192 @@
+import pygame
+import numpy as np
+import collections
+
+
+class KalmanFilter:
+
+    def __init__(self, dt: float, process_noise_std: float, measurement_noise_std: float):
+        self.dt = dt
+        self.x = np.zeros((6, 1))
+
+        dt2 = dt ** 2 / 2
+        self.A = np.array([
+            [1,   0,  dt,   0, dt2,   0],
+            [0,   1,   0,  dt,   0, dt2],
+            [0,   0,   1,   0,  dt,   0],
+            [0,   0,   0,   1,   0,  dt],
+            [0,   0,   0,   0,   1,   0],
+            [0,   0,   0,   0,   0,   1],
+        ])
+
+        self.H = np.array([
+            [1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0]
+        ])
+
+        self.Q = process_noise_std ** 2 * np.eye(6)
+        self.R = measurement_noise_std ** 2 * np.eye(2)
+        self.P = np.eye(6)
+
+    def predict(self):
+        self.x = self.A @ self.x
+        self.P = self.A @ self.P @ self.A.T + self.Q
+        return self.x[:2]
+
+    def update(self, z):
+        z = np.reshape(z, (2, 1))
+        y = z - self.H @ self.x
+        S = self.H @ self.P @ self.H.T + self.R
+        K = self.P @ self.H.T @ np.linalg.inv(S)
+        self.x = self.x + K @ y
+        self.P = (np.eye(6) - K @ self.H) @ self.P
+        return self.x[:2]
+
+class Simulator:
+
+    # dimension of the window
+    WIDTH, HEIGHT = 1024, 768
+
+    # time step in virtual units
+    DT: float = 0.1
+
+    # display scale
+    SCALE: float = 1.0
+
+    # increment of the acceleration
+    ACC_INCREMENT: float = 0.1
+
+    # limit of the acceleration
+    ACC_LIMIT: float = 1.0
+
+    # fps limit
+    FPS: int = 30
+
+    # measurement
+    MEASUREMENTS: int = FPS
+
+    # gaussian noise of process
+    process_noise_std: float = 0.02
+
+    # gaussian noise of measurements
+    measurement_noise_std: float = 10.0
+
+    def __init__(self):
+        self.pos: np.array = np.array([0.0, 0.0], dtype=np.float64)
+        self.vel: np.array = np.array([0.0, 0.0], dtype=np.float64)
+        self.acc: np.array = np.array([0.0, 0.0], dtype=np.float64)
+
+        self.kf = KalmanFilter(self.DT, self.process_noise_std, self.measurement_noise_std)
+
+        self.last_x = collections.deque(maxlen=self.MEASUREMENTS)
+        self.last_x.extend(np.zeros(self.MEASUREMENTS))
+        self.last_y = collections.deque(maxlen=self.MEASUREMENTS)
+        self.last_y.extend(np.zeros(self.MEASUREMENTS))
+
+        pygame.init()
+        pygame.font.init()
+        pygame.display.set_caption("Kalman Filter 2D Simulation")
+
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont('Sans', 20)
+
+    def loop(self):
+        running = True
+
+        while running:
+            self.screen.fill((255, 255, 255))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_RIGHT]:
+                self.acc[0] += self.ACC_INCREMENT
+            if keys[pygame.K_LEFT]:
+                self.acc[0] -= self.ACC_INCREMENT
+            if keys[pygame.K_UP]:
+                self.acc[1] += self.ACC_INCREMENT
+            if keys[pygame.K_DOWN]:
+                self.acc[1] -= self.ACC_INCREMENT
+            if keys[pygame.K_r]:
+                self.pos: np.array = np.array([0.0, 0.0], dtype=np.float64)
+                self.vel: np.array = np.array([0.0, 0.0], dtype=np.float64)
+                self.acc: np.array = np.array([0.0, 0.0], dtype=np.float64)
+                self.last_x.extend(np.zeros(self.MEASUREMENTS))
+                self.last_y.extend(np.zeros(self.MEASUREMENTS))
+            if keys[pygame.K_s]:
+                self.vel: np.array = np.array([0.0, 0.0], dtype=np.float64)
+                self.acc: np.array = np.array([0.0, 0.0], dtype=np.float64)
+            if keys[pygame.K_ESCAPE]:
+                running = False
+
+            # limit the acceleration
+            self.acc[0] = np.minimum(self.acc[0], +self.ACC_LIMIT)
+            self.acc[0] = np.maximum(self.acc[0], -self.ACC_LIMIT)
+            self.acc[1] = np.minimum(self.acc[1], +self.ACC_LIMIT)
+            self.acc[1] = np.maximum(self.acc[1], -self.ACC_LIMIT)
+
+            # simulate process noise
+            self.acc = self.acc + np.random.normal(0, self.process_noise_std, 2)
+
+            # simulate true motion
+            self.vel += self.acc * self.DT
+            self.pos += self.vel * self.DT + 0.5 * self.acc * self.DT ** 2
+
+            # Noisy measurement
+            meas = self.pos + np.random.normal(0, self.measurement_noise_std, 2)
+
+            # Kalman Filter
+            self.kf.predict()
+            estimate = self.kf.update(meas)
+
+            # last measurements
+            self.last_x.append(meas[0])
+            self.last_y.append(meas[1])
+
+            # calculate simple filtered measurements (mean)
+            mean_x = np.mean(self.last_x)
+            mean_y = np.mean(self.last_y)
+            median_x = np.median(self.last_x)
+            median_y = np.median(self.last_y)
+
+            # real position: Blue
+            pygame.draw.circle(self.screen, (0, 0, 255), self.to_screen(self.pos), 10)
+
+            # noisy measurement: Red
+            pygame.draw.circle(self.screen, (255, 0, 0), self.to_screen(meas), 2)
+
+            # estimate mean: Gray
+            pygame.draw.circle(self.screen, (32, 32, 32), self.to_screen((mean_x, mean_y)), 3)
+
+            # estimate median: Gray
+            pygame.draw.circle(self.screen, (32, 32, 32), self.to_screen((median_x, median_y)), 3)
+
+            # estimate Kalman: Green
+            pygame.draw.circle(self.screen, (0, 255, 0), self.to_screen(estimate.ravel()), 5)
+
+            # parameters
+            self.screen.blit(self.font.render(f'px: {self.pos[0]:.3f}', True, (0, 0, 0)), (5, 10))
+            self.screen.blit(self.font.render(f'py: {self.pos[1]:.3f}', True, (0, 0, 0)), (5, 40))
+            self.screen.blit(self.font.render(f'vx: {self.vel[0]:.3f}', True, (0, 0, 0)), (5, 70))
+            self.screen.blit(self.font.render(f'vy: {self.vel[1]:.3f}', True, (0, 0, 0)), (5, 100))
+            self.screen.blit(self.font.render(f'ax: {self.acc[0]:.3f}', True, (0, 0, 0)), (5, 130))
+            self.screen.blit(self.font.render(f'ay: {self.acc[1]:.3f}', True, (0, 0, 0)), (5, 160))
+
+            pygame.display.flip()
+
+            self.clock.tick(self.FPS)
+
+        pygame.quit()
+
+    @classmethod
+    def to_screen(cls, p):
+        return int(cls.WIDTH / 2.0 + p[0] * cls.SCALE), int(cls.HEIGHT / 2.0  - p[1] * cls.SCALE)
+
+
+if __name__ == '__main__':
+    simulator = Simulator()
+    simulator.loop()
